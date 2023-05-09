cf449d34e94d1f031e00446b88732807299ab2aa
AllBill127 <90201218+AllBill127@users.noreply.github.com>
2023-05-09 20:14:48 +0300

    add basic math funcs
    add addition, subtraction, multiplication and division functions

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ const dotBtn = document.querySelector(".dot");
 const operatorBtn = document.querySelectorAll(".operator");
 const zeroBtn = document.querySelector(".zero");
 
+const MAX_NUM_LENGTH = 14;
+
+// Math operation implementations
+function add1(num1, num2) {
+    return num1 + num2;
+}
+
+function sub1(num1, num2) {
+    return num1 - num2;
+}
+
+function mul1(num1, num2) {
+    return num1 * num2;
+}
+
+function div1(num1, num2) {
+    return num1 / num2;
+}
