9b7fa9cc91594eefa8d103f21a937794a354435c
AllBill127 <90201218+AllBill127@users.noreply.github.com>
2023-05-09 21:52:47 +0300

    add formatting function
    add a function to format a number so it fits to output viewport by number of digits

--- index.js
+++ index.js
@@ -24,3 +24,33 @@ function mul(num1, num2) {
 function div(num1, num2) {
     return num1 / num2;
 }
+
+// Formatting function to fit number to screen by constants
+const MAX_NUM_LENGTH = 14;  // number of digits that fit to screen
+const LARG_NUMBER = 1e14;   // what is considered a large number (should match the MAX_NUM_LENGTH)
+const MAX_EXP_LENGTH = 3; // number of digits for exponential notation 
+
+function formatNumber(x) {
+    let str = x.toString();
+    let numDigits = str.replace(".", "").length;
+    
+    if (numDigits <= MAX_NUM_LENGTH) {
+      // The number fits within the maximum number of digits
+      return str;
+    } else if (Math.abs(x) >= LARG_NUMBER) {
+      // The number is very large, use exponential notation
+      return x.toExponential(MAX_NUM_LENGTH - MAX_EXP_LENGTH);
+    } else {
+      // The number is not too large, use fixed or precision notation
+      let numIntDigits = Math.floor(Math.log10(Math.abs(x))) + 1;
+      let numFracDigits = MAX_NUM_LENGTH - numIntDigits - 1;
+      
+      if (numFracDigits < 0) {
+        // Not enough space for any fractional digits, use precision notation
+        return x.toPrecision(MAX_NUM_LENGTH - 1);
+      } else {
+        // Use fixed notation with the appropriate number of fractional digits
+        return x.toFixed(numFracDigits);
+      }
+    }
+  }
