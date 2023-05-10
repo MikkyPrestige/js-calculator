d0572e065366c41ccee055165815fef361edd2dd
Ashley-Nyasha <130581179+Ashley-Nyasha@users.noreply.github.com>
2023-05-10 05:01:59 +0200

    Update index.js
    I had to delete the whole code and I will start again as I seemed to have mixed up my methods and functions

--- index.js
+++ index.js
@@ -63,130 +63,84 @@ function formatNumber(x) {
     }
   }
 
-let curr = "";
-let prev = "";
-let operator = "";
 
-const display = () => {
-  currValue.innerText = curr;
-  prevValue.innerText = prev;
-};
-
-const clear = () => {
-  clearBtn.addEventListener("click", () => {
-    curr = "";
-    prev = "";
-    operator = "";
-    display();
-    updateDisplay()
-  });
-};
-clear();
-
-const del = () => {
-  delBtn.addEventListener("click", () => {
-    curr = curr.slice(0, -1);
-    display();
-    updateDisplay()
-  });
-};
-del();
-
-const buttons = () => {
-  numBtn.forEach((num) => {
-    num.addEventListener("click", () => {
-      if (num.classList.contains("dot")) {
-        if (curr.includes(".") || curr === "") {
-          return;
+  let curr = "";
+  let prev = "";
+  let operator = "";
+  
+  const display = () => {
+    currValue.innerText = curr;
+    prevValue.innerText = prev;
+  };
+  
+  const operation = () => {
+    operatorBtn.forEach((operator) => {
+      operator.addEventListener("click", () => {
+        if (curr === "") return;
+        if (prev !== "") {
+          prev = curr;
+        } else {
+          prev = curr;
+        }
+        curr = operator.innerText;
+        prev = curr;
+        curr = "";
+        display();
+      });
+    });
+  };
+  
+  operation();
+  
+  const buttons = () => {
+    numBtn.forEach((num) => {
+      num.addEventListener("click", () => {
+        if (num.classList.contains("dot")) {
+          if (curr.includes(".") || curr === "") {
+            return;
+          } else {
+            curr += num.innerText;
+          }
+        } else if (num.classList.contains("zero")) {
+          if (curr === "") return;
+          curr += num.innerText;
         } else {
           curr += num.innerText;
         }
-      } else if (num.classList.contains("zero")) {
-        if (curr === "") return;
-        curr += num.innerText;
-      } else {
-        curr += num.innerText;
-      }
-      display();
-      updateDisplay()
+        display();
+      });
     });
-  });
-};
-
-const operation = () => {
-  operatorBtn.forEach((operator) => {
-    operator.addEventListener("click", () => {
-      if (curr === "") return;
-      if (prev !== "") {
-        compute();
-      }
-
-      curr = operator.innerText;
-      prev = curr;
+  };
+  
+  buttons();
+  
+  const clear = () => {
+    clearBtn.addEventListener("click", () => {
       curr = "";
+      prev = "";
+      operator = "";
       display();
-      updateDisplay()
     });
-  });
-};
-operation();
-
-const compute = () => {
-equalBtn.forEach((answer) => {
-  answer.addEventListener('click', () => {
-    let computation
-  const prev = parseFloat(prevValue)
-  const current = parseFloat(currValue)
-  if (isNaN(prev) || isNaN(current)) return
-  switch (operation) {
-    case '+':
-      computation = prev + current
-      break
-    case '-':
-      computation = prev - current
-      break
-    case '*':
-      computation = prev * current
-      break
-    case '÷':
-      computation = prev / current
-      break
-    default:
-      return
-  }
-  currValue = computation
-  operation = undefined
-  prevValue = ''
-  display();
-  updateDisplay()
-  });
-});
-};
+  };
+  
+  clear();
+  
+  const del = () => {
+    delBtn.addEventListener("click", () => {
+      curr = curr.slice(0, -1);
+      display();
+    });
+  };
+  
+  del();
 
 
-// function getDisplayNumber(number) {
-//   const stringNumber = number.toString()
-//   const integerDigits = parseFloat(stringNumber.split('.')[0])
-//   const decimalDigits = stringNumber.split('.')[1]
-//   let integerDisplay
-//   if (isNaN(integerDigits)) {
-//     integerDisplay = ''
-//   } else {
-//     integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
-//   }
-//   if (decimalDigits != null) {
-//     return `${integerDisplay}.${decimalDigits}`
-//   } else {
-//     return integerDisplay
-//   }
-// }
+  const results = () => {
+    equalBtn.addEventListener("click", () => {
+      // deleted codde
+    });
+  };
 
-// function updateDisplay() {
-//   currValue.innerText = getDisplayNumber(currValue)
-//   if (operation != null) {
-//     prevValue.innerText =
-//       `${getDisplayNumber(prevValue)} ${operation}`
-//   } else {
-//     prevValue.innerText = ''
-//   }
-// }
+  results();
+
+
