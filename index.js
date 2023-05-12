033fb1c301e346aa0f41325f3f6c0b33aea467e9
AllBill127 <90201218+AllBill127@users.noreply.github.com>
2023-05-12 04:55:57 +0300

    fix spelling mistake

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function div(num1, num2) {
 
 // Formatting function to fit number to screen by constants
 const MAX_NUM_LENGTH = 14;  // number of digits that fit to screen
-const LARG_NUMBER = 1e14;   // what is considered a large number (should match the MAX_NUM_LENGTH)
+const LARGE_NUMBER = 1e14;   // what is considered a large number (should match the MAX_NUM_LENGTH)
 const MAX_EXP_LENGTH = 3; // number of digits for exponential notation 
 
 function formatNumber(x) {
@@ -43,7 +43,7 @@ function formatNumber(x) {
       // The number fits within the maximum number of digits
       console.debug('DEBUG: formatNumber(x) to just num');
       return str;
-    } else if (Math.abs(x) >= LARG_NUMBER) {
+    } else if (Math.abs(x) >= LARGE_NUMBER) {
       // The number is very large, use exponential notation
       console.debug('DEBUG: formatNumber(x) to exponential num');
       return x.toExponential(MAX_NUM_LENGTH - MAX_EXP_LENGTH);
